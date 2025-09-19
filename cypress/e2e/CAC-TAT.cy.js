751bde9e5920d72eea1b4bfe991d1679bc9feba1
Pedro Medeiros <phms.ti@gmail.com>
2025-09-19 08:39:07 -0300

    Seção 2 - Exercícios

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -104,4 +104,25 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success').should('be.visible')
   })
 
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+    .select('YouTube')
+    .should('have.value', 'youtube')
+  })
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+    .select('mentoria')
+    .should('have.value', 'mentoria')
+  })
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+    .select(1)
+    .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+    .check()
+    .should('have.value', 'feedback')
+  })
 })
