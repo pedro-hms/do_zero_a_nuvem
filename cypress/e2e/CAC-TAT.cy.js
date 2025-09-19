99923eed082b31c5e2b98d2c64c453fd99511108
Pedro Medeiros <phms.ti@gmail.com>
2025-09-19 07:14:57 -0300

    Exercício Extra 1

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,10 +10,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = 'Teste do Teste para motivos de Teste'.repeat(10)
     cy.get('#firstName').type('Pedro')
     cy.get('#lastName').type('Medeiros')
     cy.get('#email').type('pedro@pedro.com')
-    cy.get('#open-text-area').type('Teste do teste para fins de teste')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
     cy.get('.button[type="submit"').click()
     
     cy.get('.success').should('be.visible')
